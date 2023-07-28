47273e3f2dd52317d13dae5e360252f8a5ae9c0f
nagapavankumar <nagapavankumar.guggilla@gmail.com>
2023-07-28 18:06:28 +0530

    updated new next.config.js

--- next.config.js
+++ next.config.js
@@ -10,13 +10,6 @@ module.exports = {
   },
 };
 
-
-module.exports = {
-  images: {
-    unoptimized: true,
-  },
-};
-
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({
 //   enabled: process.env.ANALYZE === "true",
 // });
