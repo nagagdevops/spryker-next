0334606d8a77245a254f6a97e7beeb0b10c36891
nagapavankumar <nagapavankumar.guggilla@gmail.com>
2023-07-28 17:51:12 +0530

    updated new next.config.js

--- next.config.js
+++ next.config.js
@@ -8,8 +8,17 @@ module.exports = {
     theme: "DEFAULT",
     currency: "USD",
   },
-  output: 'export',
 };
+
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
 
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({
 //   enabled: process.env.ANALYZE === "true",
