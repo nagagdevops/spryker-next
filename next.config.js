35a4774557a4bd258de7381b80febd8ca47d0030
nagapavankumar <nagapavankumar.guggilla@gmail.com>
2023-07-28 18:02:13 +0530

    updated new next.config.js

--- next.config.js
+++ next.config.js
@@ -10,16 +10,6 @@ module.exports = {
   },
 };
 
-const nextConfig = {
-  output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
-
-module.exports = nextConfig
-
 
 module.exports = {
   images: {
