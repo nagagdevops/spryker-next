b06d75a481afc01caf244e343e4267f0a822606e
nagapavankumar <nagapavankumar.guggilla@gmail.com>
2023-07-28 19:54:38 +0530

    updated new next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 // const { i18n } = require("./next-i18next.config");
 
-module.exports = {
+//module.exports = {
   // i18n,
-  devIndicators: {},
-  publicRuntimeConfig: {
+  //devIndicators: {},
+  //publicRuntimeConfig: {
     // Available on both server and client
-    theme: "DEFAULT",
-    currency: "USD",
-    output: 'export',	  
-  },
-};
+    //theme: "DEFAULT",
+    //currency: "USD",
+   // output: 'export',	  
+  //},
+//};
 
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({
 //   enabled: process.env.ANALYZE === "true",
@@ -24,3 +24,22 @@ module.exports = {
 //     currency: "USD",
 //   },
 // });
+//
+//
+//
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //reactStrictMode: true,
+  output: 'export',
+}
+
+module.exports = nextConfig
+
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+};
+
+
